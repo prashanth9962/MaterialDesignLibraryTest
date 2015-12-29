0f31060442f0388702afcf57e051e10d4cc7f8d8
prashanth9962 <prashanth.engr@gmail.com>
2015-12-29 16:15:37 +0530

    show preference in android list

--- app/src/main/java/testapp/prashanth/com/transparentactionbartest/PreferenceSettings.java
+++ app/src/main/java/testapp/prashanth/com/transparentactionbartest/PreferenceSettings.java
@@ -3,16 +3,20 @@ package testapp.prashanth.com.transparentactionbartest;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by prashanth on 29/12/15.
  */
-public class PreferenceSettings  extends PreferenceActivity {
+public class PreferenceSettings  extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+        setContentView(R.layout.settings_activity);
+        getFragmentManager().beginTransaction()
+                .replace(R.id.container, new MyPreferenceFragment()).commit();
+        //getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
     }
 
     public static class MyPreferenceFragment extends PreferenceFragment
